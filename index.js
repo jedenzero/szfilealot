1bb5e8cff5208cbc74282562093765ed113d4059
1010 <134185805+jedenzero@users.noreply.github.com>
2024-05-19 02:39:43 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+var w=60;
+var h=20;
+function table(){
+  for(el in document.querySelectorAll('td')){
+    el.removeEventListener('click',color);
+  }
+  let i=``;
+  i+=`<table style="height:${3000/w*h}">`
+  while(h>0){
+    i+=`<tr>`;
+    while(w>0){
+      i+=`<td></td>`;
+      w--;
+    }
+    i+=`</tr>`;
+    h--;
+  }
+  i+=`</table>`;
+  document.getElementById('szfilealot').innerHTML=i;
+  for(el in document.querySelectorAll('td')){
+    el.addEventListener('click',color);
+  }
+}
+function color(el){
+  switch(this.styles.backgroundColor){
+    case 'rgb(40, 40, 40)': this.style.backgroundColor='#00FFA0'; break;
+    case 'rgb(0, 255, 160)': this.style.backgroundColor='#FFFFA0'; break;
+    case 'rgb(255, 255, 160)': this.style.backgroundColor='#282828'; break;
+  }
+}
+table();
